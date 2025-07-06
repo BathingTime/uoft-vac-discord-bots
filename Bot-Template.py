5068dc834c7124c970d1c1f7a206b5f1967cf262
Sunny Lin <sunnylin@Sunnys-MacBook-Pro-4.local>
2025-07-05 18:39:15 -0700

    Wrote a template file for Discord bots in Python. I think it's complete (for now).

---
+++ Bot-Template.py
@@ -0,0 +1,61 @@
+'''Name
+Last edited: M D, Y
+
+Description & notes.
+
+TODO: Fill out information above.
+This file is not meant to be run.
+'''
+
+import discord
+from discord.ext import commands
+
+# Constants:
+...
+
+
+intents = discord.Intents.all()
+# TODO: Set command_prefix (str) to the command prefix of choice.
+# A command prefix is the symbol at the beginning of every bot command message.
+# E.g. Use '.' for commands like ".show", or use '/' for commands like "/help".
+# '.' is the easiest in my opinion.
+client = commands.Bot(command_prefix = ..., intents = intents)
+
+# When the bot goes online.
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+
+    # Connect all auto functions.
+    client.loop.create_task(auto_func_eg)
+
+
+# Command functions: functions that fire when a Discord user types in the command name.
+# The function name is the command name.
+# E.g. If the function name is command_func_eg, then the user would type ".command_func_eg …"
+@client.command(pass_context = True)
+async def command_func_eg(ctx, *args) -> None:
+    ...
+
+...
+
+
+# Auto functions: functions that fire when the bot goes online.
+async def auto_func_eg() -> None:
+
+    # Begin only when the bot is online.
+    await client.wait_until_ready()
+
+    # Program loop; keep running unless the bot goes offline.
+    while not client.is_closed():
+        ...
+
+
+# Activate bot.
+if __name__ == '__main__':
+
+    # TODO: Enter the bot's private token (str) into the parameter of client.run.
+    # DO NOT REVEAL A BOT'S TOKEN TO ANYONE OUTSIDE THE DEV TEAM.
+    # If someone has access to a bot's token, they will be able to fully control it and cause damage to the bot and servers.
+    # If you know someone outside the dev team has access to the token (or even suspect it), regenerate a new token immediately.
+    client.run('')
