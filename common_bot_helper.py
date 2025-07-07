77a9f535ea471a1a782155ad05e2e407f0868dcb
Sunny Lin <sunnylin@Sunnys-MacBook-Pro-4.local>
2025-07-06 21:56:42 -0700

    Wrote functions to write to a txt file as well as to chop a Discord message into substrings below a given word limit. These functions are pretty complete imo.

---
+++ common_bot_helper.py
@@ -0,0 +1,91 @@
+'''Common Bot Helper
+Author: Sunny Lin
+Editor(s):
+Last edited: Jul 6, 25
+
+This file contains functions that many bots will likely find useful.
+'''
+
+def write_file(file_path: str, output: list[str]) -> None:
+    '''Write a given list of strings to a given txt file.
+    Each element in the output list is a line to be written.
+
+    Sample usage inapplicable.
+    '''
+    with open(file_path, 'w') as file_writer:
+        file_writer.write('\n'.join(output))
+
+def chop_output(output: str, limit: int) -> tuple[str]:
+    '''Given a string to be outputted, return a tuple containing chopped parts of the output
+    where the chops are at line breaks and each substring is under the given word limit.
+    This is so string above the word limit can be sent in several messages and still be displayed.
+
+    Sample Usage:
+    >>> chop_output('e', 5)
+    ['e']
+
+    >>> chop_output('eeeee', 5)
+    ['eeeee']
+
+    >>> chop_output('eeeee\\ne', 5)
+    ['eeeee', 'e']
+
+    >>> chop_output('eeeee\\neeeee', 5)
+    ['eeeee', 'eeeee']
+
+    >>> chop_output('e\\ne\\ne', 5)
+    ['e\\ne\\ne']
+
+    >>> chop_output('e\\ne\\ne\\ne', 5)
+    ['e\\ne\\ne', 'e']
+
+    >>> chop_output('e\\nee\\neee', 5)
+    ['e\\nee', 'eee']
+
+    # Does not chop if a substring itself exceeds the limit, but this is an unrealistic scenario.
+    >>> chop_output('eeeeee', 5)
+    ['eeeeee']
+    '''
+    # Split the output at all line breaks.
+    lines = output.split('\n')
+
+    # Construct the output list.
+    chopped = []
+    start = 0
+    end = 1
+
+    # Go until the end of the list.
+    while end != len(lines) + 1:
+
+        # Get the current substring.
+        substring = '\n'.join(lines[start: end])
+
+        # If the length of the substring exceeds the minimum and it contains at least one line break,
+        # append the previous substring to the output list and start from the start of the current substring.
+        if len(substring) > limit and start != end - 1:
+            chopped.append('\n'.join(lines[start: end - 1]))
+            start = end - 1
+
+        # Otherwise, the next line will be considered.
+        else:
+
+            # If the length of the substring is equal to the minimum,
+            # append the current substring to the output list and start from the end of the current substring.
+            if len(substring) == limit:
+                chopped.append(substring)
+                start = end
+
+            # Extend to the next line.
+            end += 1
+
+    # If the start index does not immediately precede the end index,
+    # then the last lines have not been added, so add them and return the output list.
+    if start != end - 1:
+        return chopped + ['\n'.join(lines[start:])]
+
+    # Otherwise, all lines have been added, so return the output list.
+    return chopped
+
+if __name__ == '__main__':
+    from doctest import testmod
+    testmod()
